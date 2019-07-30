d6260d744f8222468376a5f6b8b8d8cb4f3caf79
Michele <skumbagmeno@gmail.com>
2019-07-30 21:23:32 +0200

    Altra roba

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php 
+$inote = '<svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M336 64h-80c0-35.3-28.7-64-64-64s-64 28.7-64 64H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zM192 40c13.3 0 24 10.7 24 24s-10.7 24-24 24-24-10.7-24-24 10.7-24 24-24zm144 418c0 3.3-2.7 6-6 6H54c-3.3 0-6-2.7-6-6V118c0-3.3 2.7-6 6-6h42v36c0 6.6 5.4 12 12 12h168c6.6 0 12-5.4 12-12v-36h42c3.3 0 6 2.7 6 6z" class=""></path></svg>';
+?>
 <html>
     <head>
         <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap" rel="stylesheet">
@@ -8,15 +11,41 @@
             <div class="grid">
                 <?php
                 for ($i = 0; $i < 20; $i++) { ?>
-                    <div class="row bg-red">
-                        <div class="cell cell-1 bg-green">
+                    <div class="row">
+                        <div class="cell cell-1">
                             <label class="checkbox">
                                 <input type="checkbox">
                                 <span class="mark"></span>
                             </label>
                         </div>
-                        <div class="cell cell-3">
-                            <div class="titolo lg">CEEPORT SRL</div>
+                        <div class="cell cell-2">
+                            <div class="titolo sm blue light">CEEPORT SRL</div>
+                        </div>
+                        <div class="cell">
+                            <div class="sm">12/02/2019</div>
+                        </div>
+                        <div class="cell">
+                            <div class="sm">12/02/2019</div>
+                        </div>
+                        <div class="cell">
+                            <div class="sm">1.254,00</div>
+                        </div>
+                        <div class="cell">
+                            <div class="sm">124,00</div>
+                        </div>
+                        <div class="cell">
+                            <div class="md blue light bold">1.584,00</div>
+                        </div>
+                        <div class="cell">
+                            <div class="sm">0,00</div>
+                        </div>
+                        <div class="cell">
+                            <select>
+                                <option>Valore</option>
+                            </select>
+                        </div>
+                        <div class="cell">
+                            <?= $inote ?>
                         </div>
                     </div>
                 <?php
